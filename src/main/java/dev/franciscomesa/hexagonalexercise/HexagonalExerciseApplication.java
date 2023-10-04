10e1c5898054dfc149db2a6bba83f84bbe68a959
Francisco Mesa <franciscomesa@gmail.com>
2023-10-04 10:41:35 +0100

    :tada: First commit after IntelliJ wizard

---
+++ src/main/java/dev/franciscomesa/hexagonalexercise/HexagonalExerciseApplication.java
@@ -0,0 +1,13 @@
+package dev.franciscomesa.hexagonalexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/franciscomesa/hexagonalexercise/HexagonalExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.franciscomesa.hexagonalexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
