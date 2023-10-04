e3b01d4427a932ac62098138bfc3ccd1b78624dd
Francisco Mesa <franciscomesa@gmail.com>
2023-10-04 11:16:06 +0100

    :construction: Add Acceptance tests

---
+++ src/test/java/dev/franciscomesa/hexagonalexercise/AcceptanceTests.java
@@ -0,0 +1,39 @@
+package dev.franciscomesa.hexagonalexercise;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AcceptanceTests {
+
+    @Disabled
+    @Test
+    void request_at_TenOClock_At_day14_from_product_34544_should_returnX () {
+        // Test 1: petición a las 10:00 del día 14 del producto 35455   para la brand 1 (A)
+    }
+
+    @Disabled
+    @Test
+    void request_at_SixteenOClock_At_day14_from_product_34544_should_returnX() {
+        // Test 2: petición a las 16:00 del día 14 del producto 35455   para la brand 1 (A)
+    }
+
+    @Disabled
+    @Test
+    void request_at_TwentyOneOClock_At_day14_from_product_34544_should_returnX() {
+        // Test 3: petición a las 21:00 del día 14 del producto 35455   para la brand 1 (A)
+    }
+
+    @Disabled
+    @Test
+    void request_at_TenOClock_At_day15_from_product_34544_should_returnX() {
+        // Test 4: petición a las 10:00 del día 15 del producto 35455   para la brand 1 (A)
+    }
+
+    @Disabled
+    @Test
+    void request_at_TwentyOneOClock_At_day15_from_product_34544_should_returnX() {
+        // Test 5: petición a las 21:00 del día 16 del producto 35455   para la brand 1 (A)
+    }
+}
