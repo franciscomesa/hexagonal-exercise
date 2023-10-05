5d919372ef8c73399d043f38dbfa0fd2343cfb98
Francisco Mesa <franciscomesa@gmail.com>
2023-10-05 19:56:54 +0100

    :white_check_mark: Add ProductPrice tests

---
+++ src/main/java/dev/franciscomesa/hexagonalexercise/productprice/domain/model/ProductPrice.java
@@ -0,0 +1,55 @@
+package dev.franciscomesa.hexagonalexercise.productprice.domain.model;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class ProductPrice {
+    final public Long productId;
+    final public Long brandId;
+    final public Long priceList;
+    @Getter
+    final public Long priority;
+    final public LocalDateTime startDate;
+    final public LocalDateTime endDate;
+    final public Double price;
+
+    private ProductPrice(Long productId, Long brandId, Long priceList, Long priority, LocalDateTime startDate, LocalDateTime endDate, Double price) {
+        this.productId = productId;
+        this.brandId = brandId;
+        this.priceList = priceList;
+        this.priority = priority;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.price = price;
+    }
+
+    public static ProductPrice builder(Long productId, Long brandId, Long priceList, Long priority, LocalDateTime startDate, LocalDateTime endDate, Double price) {
+        return new ProductPrice(
+            productId,
+            brandId,
+            priceList,
+            priority,
+            startDate,
+            endDate,
+            price
+        );
+    }
+
+    public static Optional<ProductPrice> getAppliedProductPrice(List<ProductPrice> productPrices, LocalDateTime applicationDate) {
+        return productPrices.stream()
+                .filter(applicationDateBetweenStartAndEndDates(applicationDate)                )
+                .max(Comparator.comparing(ProductPrice::getPriority));
+
+    }
+
+    private static Predicate<ProductPrice> applicationDateBetweenStartAndEndDates(LocalDateTime applicationDate) {
+        return price -> price.startDate.isBefore(applicationDate) && price.endDate.isAfter(applicationDate)
+                || price.startDate.isEqual(applicationDate) || price.endDate.isEqual(applicationDate);
+    }
+
+}
---
+++ src/test/java/dev/franciscomesa/hexagonalexercise/ProductPriceTests.java
@@ -0,0 +1,87 @@
+package dev.franciscomesa.hexagonalexercise;
+
+import dev.franciscomesa.hexagonalexercise.productprice.domain.model.ProductPrice;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class ProductPriceTests {
+    static List<ProductPrice> productPrices;
+
+
+    @BeforeAll
+    public static void fixturesProductPrice() {
+        long lowerPriority = 0;
+        long higherPriority = 1;
+        ProductPrice productPrice1 = buildFixture(lowerPriority, "2024-01-06 00.00.00", "2024-12-31 23.59.59",1.00);
+        ProductPrice productPrice2 = buildFixture(higherPriority, "2024-01-06 15.00.00", "2024-01-06 18.30.00",2.00);
+        productPrices = new ArrayList<>();
+        productPrices.add(productPrice1);
+        productPrices.add(productPrice2);
+    }
+
+
+    @Test
+    public void productPrice_return_a_value_if_applied_date_is_start_date() {
+        Optional<ProductPrice> productPrice = ProductPrice
+                .getAppliedProductPrice(productPrices, ProductPriceTests.stringToLocalDateTimeHelper("2024-01-06 00.01.00"));
+
+        assertTrue(productPrice.isPresent());
+    }
+
+    @Test
+    public void productPrice_return_a_value_if_applied_date_is_end_date() {
+        Optional<ProductPrice> productPrice = ProductPrice
+                .getAppliedProductPrice(productPrices, ProductPriceTests.stringToLocalDateTimeHelper("2024-12-31 23.59.59"));
+
+        assertTrue(productPrice.isPresent());
+    }
+
+    @Test
+    public void productPrice_return_a_value_if_applied_date_is_between_start_and_end_dates() {
+        Optional<ProductPrice> productPrice = ProductPrice
+                .getAppliedProductPrice(productPrices, ProductPriceTests.stringToLocalDateTimeHelper("2024-02-02 00.00.00"));
+
+        assertTrue(productPrice.isPresent());
+        assertEquals(productPrice.get().price, 1.0);
+    }
+
+    @Test
+    public void productPrice_return_the_price_with_highest_priority_if_two_prices_in_applied_date() {
+        Optional<ProductPrice> productPrice = ProductPrice
+                .getAppliedProductPrice(productPrices, ProductPriceTests.stringToLocalDateTimeHelper("2024-01-06 17.00.00"));
+
+        assertTrue(productPrice.isPresent());
+        assertEquals(productPrice.get().price, 2.0);
+    }
+
+    private static ProductPrice buildFixture(long priority, String startDate, String endDate, double price) {
+        long DEFAULT_PRODUCT = 1;
+        long DEFAULT_BRAND = 1;
+        long DEFAULT_PRICE_LIST = 1;
+
+        return ProductPrice.builder(
+                DEFAULT_PRODUCT,
+                DEFAULT_BRAND,
+                DEFAULT_PRICE_LIST,
+                priority,
+                ProductPriceTests.stringToLocalDateTimeHelper(startDate),
+                ProductPriceTests.stringToLocalDateTimeHelper(endDate),
+                price);
+    }
+
+    private static LocalDateTime stringToLocalDateTimeHelper(String date) {
+        DateTimeFormatter dateFormatterHelper = DateTimeFormatter.ofPattern("yyyy-MM-dd HH.mm.ss");
+        return LocalDateTime.parse(date, dateFormatterHelper);
+    }
+}
