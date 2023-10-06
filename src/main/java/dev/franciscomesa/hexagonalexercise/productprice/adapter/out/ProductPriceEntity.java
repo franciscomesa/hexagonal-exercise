b4c75fb5c79899098309c00b2e5e12f954e03e25
Francisco Mesa <franciscomesa@gmail.com>
2023-10-06 09:30:10 +0100

    :sparkles: Add ProductPriceEntity

---
+++ src/main/java/dev/franciscomesa/hexagonalexercise/productprice/adapter/out/ProductPriceEntity.java
@@ -0,0 +1,59 @@
+package dev.franciscomesa.hexagonalexercise.productprice.adapter.out;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class ProductPriceEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private Long productId;
+    private Long brandId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Long priority;
+    private Long priceList;
+    private Double price;
+    private String currency;
+
+    private ProductPriceEntity(Long productId, Long brandId, LocalDateTime startDate, LocalDateTime endDate, Long priceList, Long priority, Double price, String currency) {
+        this.productId = productId;
+        this.brandId = brandId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.priceList = priceList;
+        this.priority = priority;
+        this.price = price;
+        this.currency = currency;
+    }
+
+    public ProductPriceEntity() {
+    }
+
+    public static ProductPriceEntity createPrice(Long productId, Long brandId, LocalDateTime startDate, LocalDateTime endDate, Long priceList, Long priority, Double price, String currency) {
+        return new ProductPriceEntity(productId, brandId, startDate, endDate, priceList, priority, price, currency);
+    }
+
+
+    @Override
+    public String toString() {
+        return "ProductPrice{" +
+                "id=" + id +
+                ", productId=" + productId +
+                ", brandId=" + brandId +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", priceList=" + priceList +
+                ", priority=" + priority +
+                ", price=" + price +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
