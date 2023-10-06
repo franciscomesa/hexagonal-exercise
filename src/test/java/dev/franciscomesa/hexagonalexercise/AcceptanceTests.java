a37d52fc17a45f8adbe91bde89f146d054ea297d
Francisco Mesa <franciscomesa@gmail.com>
2023-10-06 10:19:10 +0100

    :recycle: Avoid repeat params at test methods

--- src/test/java/dev/franciscomesa/hexagonalexercise/AcceptanceTests.java
+++ src/test/java/dev/franciscomesa/hexagonalexercise/AcceptanceTests.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -22,12 +24,12 @@ public class AcceptanceTests {
 
     @Test
     void request_at_TenOClock_At_day14_from_product_34544_should_returnX () throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api")
-                        .param("productId", String.valueOf(this.PRODUCT_ID))
-                        .param("brandId", String.valueOf(this.BRAND_ID))
-                        .param("applicationDate", "2020-06-14T10:00:00")
-                        .contentType(MediaType.APPLICATION_JSON))
+        MockHttpServletRequestBuilder request = findPriceControllerBuilder()
+                        .param("applicationDate", "2020-06-14T10:00:00");
+
+        ResultActions result = mockMvc.perform(request);
+
+        result
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.*", hasSize(7)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.productId").value(35455))
@@ -37,12 +39,12 @@ public class AcceptanceTests {
 
     @Test
     void request_at_SixteenOClock_At_day14_from_product_34544_should_returnX() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api")
-                        .param("productId", String.valueOf(this.PRODUCT_ID))
-                        .param("brandId", String.valueOf(this.BRAND_ID))
-                        .param("applicationDate", "2020-06-14T16:00:00")
-                        .contentType(MediaType.APPLICATION_JSON))
+        MockHttpServletRequestBuilder request = findPriceControllerBuilder()
+                        .param("applicationDate", "2020-06-14T16:00:00");
+
+        ResultActions result = mockMvc.perform(request);
+
+        result
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.*", hasSize(7)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.productId").value(35455))
@@ -52,12 +54,12 @@ public class AcceptanceTests {
 
     @Test
     void request_at_TwentyOneOClock_At_day14_from_product_34544_should_returnX() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api")
-                        .param("productId", String.valueOf(this.PRODUCT_ID))
-                        .param("brandId", String.valueOf(this.BRAND_ID))
-                        .param("applicationDate", "2020-06-14T21:00:00")
-                        .contentType(MediaType.APPLICATION_JSON))
+        MockHttpServletRequestBuilder request = findPriceControllerBuilder()
+                .param("applicationDate", "2020-06-14T21:00:00");
+
+        ResultActions result = mockMvc.perform(request);
+
+        result
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.*", hasSize(7)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.productId").value(35455))
@@ -67,12 +69,12 @@ public class AcceptanceTests {
 
     @Test
     void request_at_TenOClock_At_day15_from_product_34544_should_returnX() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api")
-                        .param("productId", String.valueOf(this.PRODUCT_ID))
-                        .param("brandId", String.valueOf(this.BRAND_ID))
-                        .param("applicationDate", "2020-06-15T10:00:00")
-                        .contentType(MediaType.APPLICATION_JSON))
+        MockHttpServletRequestBuilder request = findPriceControllerBuilder()
+                .param("applicationDate", "2020-06-15T10:00:00");
+
+        ResultActions result = mockMvc.perform(request);
+
+        result
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.*", hasSize(7)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.productId").value(35455))
@@ -82,16 +84,24 @@ public class AcceptanceTests {
 
     @Test
     void request_at_TwentyOneOClock_At_day15_from_product_34544_should_returnX() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api")
-                        .param("productId", String.valueOf(this.PRODUCT_ID))
-                        .param("brandId", String.valueOf(this.BRAND_ID))
-                        .param("applicationDate", "2020-06-16T21:00:00")
-                        .contentType(MediaType.APPLICATION_JSON))
+        MockHttpServletRequestBuilder request = findPriceControllerBuilder()
+                .param("applicationDate", "2020-06-15T21:00:00");
+
+        ResultActions result = mockMvc.perform(request);
+
+        result
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.*", hasSize(7)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.productId").value(35455))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.brandId").value(1))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(38.95));
+    }
+
+    private MockHttpServletRequestBuilder findPriceControllerBuilder() {
+        return MockMvcRequestBuilders
+                .get("/api")
+                .param("productId", String.valueOf(this.PRODUCT_ID))
+                .param("brandId", String.valueOf(this.BRAND_ID))
+                .contentType(MediaType.APPLICATION_JSON);
     }
 }
